cb80de70fe7d987aff09bbbb7997e0d97a61f6f0
Swagnik Dutta <swagnikd@gmail.com>
2021-10-28 01:17:03 +0530

    minor changes, added comments, refactored a bit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,54 @@
 import React, { Component } from 'react';
-import raw from '../data/data2.json';
+import raw from '../data/data.json';
 
 import "./main.css";
 
 class Main extends Component {
 
   hasChildren = (comment) => {
+    /**
+     * Sure there's a better way to do this which is to use lodash get
+     */
     return comment.replies 
       && comment.replies.data 
       && comment.replies.data.children 
       && comment.replies.data.children.length;
   }
 
-  renderComment = (comment, depth) => {
+  hasNoChildren = (comment) => !this.hasChildren(comment)
 
-    // comment has children
-    if (this.hasChildren(comment)) {
-      const children = comment.replies.data.children.map((c) => {
-        return this.renderComment(c.data, depth + 1)
-      });
-
-      return (
-        <div key={comment.id} className="comment">
-          <div>{comment.body}</div>
-          <div style={{ paddingLeft: 20 }}>{children}</div>
-        </div>
-      )      
-    } else {
+  renderComment = (comment) => {
+    /**
+     * If comment has no children
+     */
+    if (this.hasNoChildren(comment)) {
       return (
         <div key={comment.id} className="comment">
           <div>{comment.body}</div>
         </div>
       )
     }
+
+    /**
+     * Comment has children
+     */
+    const children = comment.replies.data.children.map((c) => {
+      return this.renderComment(c.data)
+    });
+    
+    return (
+      <div key={comment.id} className="comment">
+        <div>{comment.body}</div>
+        <div className="children-list">{children}</div>
+      </div>
+    )
   }
 
   render() {
     const comments = raw[1].data.children
     return (
       <div>
-        { comments.map((comment) => this.renderComment(comment.data, 0)) }
+        { comments.map((comment) => this.renderComment(comment.data)) }
       </div>
     );
   }
